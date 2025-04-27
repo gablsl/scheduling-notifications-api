6495262fbec0fa46560864a010da8700090d2fe2
gablsl <gabrielsousadeveloper@gmail.com>
2025-04-27 17:44:01 -0300

    feat: first commit

---
+++ src/test/java/com/gabriel/schedulingapi/SchedulingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.schedulingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
