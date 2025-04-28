b5cc8923c6a6efbb0bc6ef1419f546d4810e6926
gablsl <gabrielsousadeveloper@gmail.com>
2025-04-28 12:08:04 -0300

    feat: add notifications search methods

--- src/main/java/com/gabriel/schedulingapi/business/SchedulingService.java
+++ src/main/java/com/gabriel/schedulingapi/business/SchedulingService.java
@@ -3,6 +3,7 @@ package com.gabriel.schedulingapi.business;
 import com.gabriel.schedulingapi.business.mapper.ISchedulingMapper;
 import com.gabriel.schedulingapi.controller.dto.in.SchedulingRecordInput;
 import com.gabriel.schedulingapi.controller.dto.out.SchedulingRecordOutput;
+import com.gabriel.schedulingapi.infra.exceptions.NotFoundException;
 import com.gabriel.schedulingapi.infra.repositories.SchedulingRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,5 +19,10 @@ public class SchedulingService {
                 schedulingRepository.save(
                         schedulingMapper.forEntity(
                                 schedulingRecordInput)));
+    }
+
+    public SchedulingRecordOutput findById(Long id) {
+        return schedulingMapper.forDto(schedulingRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Id not found")));
     }
 }
